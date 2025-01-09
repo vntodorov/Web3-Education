844d7761fc63fe1f1d487b38076a71ce6da1390b
vntodorov <vntodorov02@gmail.com>
2025-01-09 23:38:53 +0200

    Add events

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -14,21 +14,37 @@ contract Riffle {
 
     uint256 private immutable i_entranceFee;
 
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
+
     address payable[] private s_players;
 
-    constructor(uint256 entranceFee) {
+    uint256 private s_lastTimeStamp;
+
+    /* Events */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
         if (msg.value > i_entranceFee) {
             revert Raffle_SendMoreToEnterRaffle();
         }
 
         s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
 
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
