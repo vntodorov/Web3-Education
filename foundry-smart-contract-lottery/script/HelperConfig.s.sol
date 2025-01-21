19896c042347bfa3eaf689be643342c150f887fd
vntodorov <vntodorov02@gmail.com>
2025-01-21 23:30:45 +0200

    Finish deploy script

--- foundry-smart-contract-lottery/script/HelperConfig.s.sol
+++ foundry-smart-contract-lottery/script/HelperConfig.s.sol
@@ -48,6 +48,10 @@ contract HelperConfig is CodeConstants, Script {
         }
     }
 
+    function getConfig() public returns (NetworkConfig memory) {
+        return getConfigByChainId(block.chainid);
+    }
+
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
         return
             NetworkConfig({
