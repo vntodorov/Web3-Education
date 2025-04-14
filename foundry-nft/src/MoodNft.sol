4eb753d32a1db60cde0af33ae3dcaf49c671948b
vntodorov <vntodorov02@gmail.com>
2025-04-14 16:09:52 +0300

    Flip nft mood

--- foundry-nft/src/MoodNft.sol
+++ foundry-nft/src/MoodNft.sol
@@ -6,6 +6,8 @@ import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 
 contract MoodNft is ERC721 {
+    error MoodNft__CantFlipMoofIfNotOwner();
+
     uint256 private s_tokenCounter;
     string private s_happySvgImageUri;
     string private s_sadSvgImageUri;
@@ -30,6 +32,18 @@ contract MoodNft is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenIdToMood[s_tokenCounter] = Mood.HAPPY;
         s_tokenCounter = s_tokenCounter + 1;
+    }
+
+    function flipMood(uint256 tokenId) public {
+        if (!_isAuthorized(_ownerOf(tokenId), msg.sender, tokenId)) {
+            revert MoodNft__CantFlipMoofIfNotOwner();
+        }
+
+        if (s_tokenIdToMood[tokenId] == Mood.HAPPY) {
+            s_tokenIdToMood[tokenId] = Mood.SAD;
+        } else {
+            s_tokenIdToMood[tokenId] = Mood.HAPPY;
+        }
     }
 
     function _baseURI() internal pure override returns (string memory) {
